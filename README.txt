b17e7f0c4db3d0241e7e66688b34952694f6fcf9
dragonfly9999 <henwir42@gmail.com>
2023-10-20 16:49:49 +0700

    nop bai tap

---
+++ README.txt
@@ -0,0 +1,13 @@
+Thông tin nhóm : NHÓM 8: 
+Thành viên 1: Ngô Ngọc Long (Home Page, Elements Page)
+Thành viên 2: Thanh Thy (About-us Page, Contact Page)
+Các phần đã làm gồm : Home, About us, Contact, Elements Page;
+
+GiỚI THIỆU SƠ VỀ WEBSITE
+*Công nghệ & thư viện sử dụng:
+Front-end: 
+- Bootstrap
+- Google Fonts
+- Font Awesome
+- Magnific-Popup 
+- Count-up Jquery
---
+++ js/QLJS/index.js
@@ -0,0 +1,34 @@
+// thanh nav co giãn
+
+window.onscroll = function () {
+    myFunction();
+  };
+  
+  var header = document.getElementById("navFix");
+  var sticky = header.offsetTop;
+  
+  function myFunction() {
+    if (window.pageYOffset === 0) {
+      header.classList.remove("sticky-theme");
+    } else {
+      header.classList.add("sticky-theme");
+    }
+  }
+  
+  // chế độ sáng tối
+  var nutToi = document.getElementById("cheDoToi");
+  var nutSang = document.getElementById("cheDoSang");
+  nutSang.style.display = "none";
+  
+  document.getElementById('cheDoToi').onclick = function(){
+    document.getElementById("bodyOf-Studious").classList.remove("giaoDienSang");
+    document.getElementById("bodyOf-Studious").classList.add("giaoDienToi");
+    nutSang.style.display = 'inline-block'
+    nutToi.style.display = 'none'
+  }
+  document.getElementById('cheDoSang').onclick = function(){
+    document.getElementById("bodyOf-Studious").classList.remove("giaoDienToi");
+    document.getElementById("bodyOf-Studious").classList.add("giaoDienSang");
+    nutSang.style.display = 'none'
+    nutToi.style.display = 'inline-block'
+  }
